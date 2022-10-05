31067be7868dfc2d21d779ed70baab08dca7428d
nguyenthimai5 <mikhailthv@gmail.com>
2022-10-05 15:48:39 +0700

    MD2_BAI7_BT

--- src/BT2/Main.java
+++ src/BT2/Main.java
@@ -2,7 +2,9 @@ package BT2;
 
 public class Main {
     public static void main(String[] args) {
+        Square[] squares=new Square[1];
         Square square=new Square(3.3);
+      squares[0]=square;
         System.out.println("AreaSquare: "+square.getAreaSquare()+"\n"+square.howToColor());
     }
 }
